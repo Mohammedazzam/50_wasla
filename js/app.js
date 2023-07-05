c2baf60a52e19110653e652ee11df69ea9a85049
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-07-05 03:18:53 +0300

    edit all file

--- js/app.js
+++ js/app.js
@@ -9,35 +9,25 @@ $(function () {
   });
 
 
-    // scroll to top
-        
-    $('.scroll-top').click(function () {
-      $('html, body').animate({
-          scrollTop: 0
-      }, 100);
-    });
-    /// hide & show scroll to top
-
-    $(window).scroll(function () {
-      var clickToScroll = $('.scroll-top');
-      if ($(window).scrollTop() > 600) {
-          if (clickToScroll.is(':hidden')) {
-              clickToScroll.fadeIn(500);
-          }
-      } else {clickToScroll.fadeOut(500); }
+  // scroll to top
       
-    });
+  $('.scroll-top').click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 100);
+  });
+  /// hide & show scroll to top
 
-    // set image of carousel dots
-    $(document).ready(function(){
-      $('#campaings-carousel .owl-dot').each(function(){
-        var index = $(this).index();
-        var img = $('.campaing-item:nth('+index+') .bg-div').attr('data-img');
-        console.log(img);
-        $(this).append('<img src="'+img+'">');
-        // button.html(content);
-      });
-    });
+  $(window).scroll(function () {
+    var clickToScroll = $('.scroll-top');
+    if ($(window).scrollTop() > 600) {
+        if (clickToScroll.is(':hidden')) {
+            clickToScroll.fadeIn(500);
+        }
+    } else {clickToScroll.fadeOut(500); }
+    
+  });
+
     // stop video paly in modal fade out
     $('#videoModal').modal({
         show: false
@@ -46,12 +36,12 @@ $(function () {
     });
 
 
-  $(".datetimepicker").each(function () {
-      $(this).datetimepicker({
-        timepicker:false,
-        // format:'H:i',
-      });
-  });
+    $('.datetimepicker').datepicker({
+      format:'dd/mm/yyyy',
+      language: "ar",
+      todayHighlight:true,
+      weekStart:6
+    });
   
   $(".option").click(function(){
     $(".option").removeClass("active");
@@ -60,16 +50,8 @@ $(function () {
   });
 
   // favorate heart click
-  $('.heart').click(function(){
-    var id = $(this).attr('id');
-    console.log(id);
-    if(id == 'heart-unchecked'){
-      $('#heart-checked').show();
-      $('#heart-unchecked').hide();
-    }else if(id == 'heart-checked'){
-        $('#heart-checked').hide();
-        $('#heart-unchecked').show();
-    }
+  $('.heart,.star').click(function(){
+    $(this).toggleClass('checked');
   });
   
   // login page set
@@ -89,6 +71,150 @@ $(function () {
       $('#pass').attr('type', 'password');
       $(this).removeClass('checked');
     }
+  });
+
+
+  
+  $(document).ready(function(){
+    $('#main-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('#social-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('#partners-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $('#header-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        dots: true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    $('#campaings-carousel').owlCarousel({
+        margin:20,
+        dots: true,
+        nav:true,
+        //dotsData: true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    $('#center-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      dots: true,
+      nav:true,
+      autoplay:true,
+      autoplayTimeout:2000,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+    });
+    $('#blog-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:false,
+      autoplay:true,
+      autoplayTimeout:2000,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+    });
+  });
+
+  
+  // set image of carousel dots
+  $(document).ready(function(){
+    $('#campaings-carousel:not(.center-carousel) .owl-dot').each(function(){
+      var index = $(this).index();
+      var img = $('.campaing-item:nth('+index+') .bg-div').attr('data-img');
+      console.log(img);
+      $(this).append('<img src="'+img+'">');
+      // button.html(content);
+    });
   });
 
 });
